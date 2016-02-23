1d8b23ab4fbbd6b6531ec76bc9354e117ab99bfb
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-02-23 13:11:25 +0000

    [HelloWorld] New sample added.

---
+++ HelloWorld/HelloWorld/HomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+		}
+
+		void OnValueChanged (object sender, ValueChangedEventArgs args)
+		{
+			valueLabel.Text = args.NewValue.ToString ("F3");
+		}
+
+		async void OnButtonClicked (object sender, EventArgs args)
+		{
+			await DisplayAlert ("Clicked", "The button was clicked", "OK");
+		}
+	}
+}
+
