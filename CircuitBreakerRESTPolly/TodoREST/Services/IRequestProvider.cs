c2910ed694b6d19bcbbe62e45d8cb8368c295ba5
David Britch <davidbritch@Davids-MacBook-Pro.local>
2017-11-23 11:34:12 +0000

    New sample added.

---
+++ CircuitBreakerRESTPolly/TodoREST/Services/IRequestProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace TodoREST
+{
+    public interface IRequestProvider
+    {
+        Task<TResult> GetAsync<TResult>(string uri);
+        Task<bool> PostAsync<TResult>(string uri, TResult data);
+        Task<bool> PutAsync<TResult>(string uri, TResult data);
+        Task<bool> DeleteAsync(string uri);
+    }
+}
