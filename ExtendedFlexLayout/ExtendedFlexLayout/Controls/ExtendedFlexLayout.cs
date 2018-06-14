dcd6b1de6e7430e40213f705f89f8c9a66bc4a6a
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-06-14 11:55:10 +0100

    Sample added.

---
+++ ExtendedFlexLayout/ExtendedFlexLayout/Controls/ExtendedFlexLayout.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using Xamarin.Forms;
+
+namespace ExtendedFlexLayout
+{
+    public class ExtendedFlexLayout : FlexLayout
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSourceProperty), typeof(IEnumerable), typeof(ExtendedFlexLayout), propertyChanged: OnItemsSourceChanged);
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(nameof(ItemTemplateProperty), typeof(DataTemplate), typeof(ExtendedFlexLayout));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        static void OnItemsSourceChanged(BindableObject bindable, object oldVal, object newVal)
+        {
+            IEnumerable newValue = newVal as IEnumerable;
+            var layout = (ExtendedFlexLayout)bindable;
+
+            layout.Children.Clear();
+            if (newValue != null)
+            {
+                foreach (var item in newValue)
+                {
+                    layout.Children.Add(layout.CreateChildView(item));
+                }
+            }
+        }
+
+        View CreateChildView(object item)
+        {
+            ItemTemplate.SetValue(BindableObject.BindingContextProperty, item);
+            return (View)ItemTemplate.CreateContent();
+        }
+    }
+}
---
+++ ExtendedFlexLayout/ExtendedFlexLayout/Models/Monkey.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtendedFlexLayout
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Trait1 { get; set; }
+        public string Trait2 { get; set; }
+        public string Trait3 { get; set; }
+        public string Image { get; set; }
+    }
+}
