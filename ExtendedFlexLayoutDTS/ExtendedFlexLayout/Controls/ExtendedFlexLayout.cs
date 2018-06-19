790c1d6290e028cc4efc853901115f43b4da99bc
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-06-19 08:47:29 +0100

    New sample.

---
+++ ExtendedFlexLayoutDTS/ExtendedFlexLayout/Controls/ExtendedFlexLayout.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using Xamarin.Forms;
+
+namespace ExtendedFlexLayout
+{
+    public class ExtendedFlexLayout : FlexLayout
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable), typeof(ExtendedFlexLayout), propertyChanged: OnItemsSourceChanged);
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(nameof(ItemTemplate), typeof(DataTemplate), typeof(ExtendedFlexLayout));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        static void OnItemsSourceChanged(BindableObject bindable, object oldVal, object newVal)
+        {
+            IEnumerable newValue = newVal as IEnumerable;
+            var layout = (ExtendedFlexLayout)bindable;
+
+            layout.Children.Clear();
+            if (newValue != null)
+            {
+                foreach (var item in newValue)
+                {
+                    layout.Children.Add(layout.CreateChildView(item));
+                }
+            }
+        }
+
+        View CreateChildView(object item)
+        {
+            if (ItemTemplate is DataTemplateSelector)
+            {
+                var dts = ItemTemplate as DataTemplateSelector;
+                var itemTemplate = dts.SelectTemplate(item, null);
+                itemTemplate.SetValue(BindableObject.BindingContextProperty, item);
+                return (View)itemTemplate.CreateContent();
+            }
+            else
+            {
+                ItemTemplate.SetValue(BindableObject.BindingContextProperty, item);
+                return (View)ItemTemplate.CreateContent();
+            }
+        }
+    }
+}
---
+++ ExtendedFlexLayoutDTS/ExtendedFlexLayout/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ExtendedFlexLayout
+{
+    public class MainPageViewModel
+    {
+        public IList<Monkey> Monkeys { get; private set; }
+
+        public MainPageViewModel()
+        {
+            Monkeys = new List<Monkey>();
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Seated Monkey",
+                Description = "This monkey is laid back and relaxed, and likes to watch the world go by.",
+                Trait1 = "• Doesn't make a lot of noise",
+                Trait2 = "• Often smiles mysteriously",
+                Trait3 = "• Sleeps sitting up",
+                Image = "ExtendedFlexLayout.Images.SeatedMonkey.jpg",                 
+            });
+            Monkeys.Add(new Monkey
+            {
+                Name = "Banana Monkey",
+                Description = "Watch this monkey eat a giant banana.",
+                Trait1 = "• More fun than a barrel of monkeys",
+                Trait2 = "• Banana not included",
+                Image = "ExtendedFlexLayout.Images.Banana.jpg"
+            });
+            Monkeys.Add(new Monkey
+            {
+                Name = "Face-Palm Monkey",
+                Description = "This monkey reacts appropriately to ridiculous assertions and actions.",
+                Trait1 = "• Cynical but not unfriendly",
+                Trait2 = "• Seven varieties of grimaces",
+                Trait3 = "• Doesn't laugh at your jokes",
+                Image = "ExtendedFlexLayout.Images.FacePalm.jpg"
+            });
+        }
+    }
+}
