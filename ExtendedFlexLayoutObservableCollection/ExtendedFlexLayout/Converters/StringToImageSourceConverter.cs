3f2bd162b76c599cb7d5ff75eef3d21605d56cd9
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-07-02 15:25:25 +0100

    New sample.

---
+++ ExtendedFlexLayoutObservableCollection/ExtendedFlexLayout/Converters/StringToImageSourceConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ExtendedFlexLayout
+{
+    public class StringToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? ImageSource.FromResource(value as string) : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
