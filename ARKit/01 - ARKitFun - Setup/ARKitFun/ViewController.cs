5e50aaa568c408c44ecf45c4f88e338ed7d753e6
David Britch <davidbritch@users.noreply.github.com>
2021-03-15 10:21:12 +0000

    ARKit setup.

---
+++ ARKit/01 - ARKitFun - Setup/ARKitFun/ViewController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ARKit;
+using UIKit;
+
+namespace ARKitFun
+{
+    public partial class ViewController : UIViewController
+    {
+        readonly ARSCNView sceneView;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            sceneView = new ARSCNView
+            {
+                AutoenablesDefaultLighting = true,
+                ShowsStatistics = true
+            };
+            View.AddSubview(sceneView);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            sceneView.Frame = View.Frame;
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            sceneView.Session.Run(new ARWorldTrackingConfiguration
+            {
+                AutoFocusEnabled = true,
+                LightEstimationEnabled = true,
+                WorldAlignment = ARWorldAlignment.Gravity
+            }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            sceneView.Session.Pause();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+    }
+}
