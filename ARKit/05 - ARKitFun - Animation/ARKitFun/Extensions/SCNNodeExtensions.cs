ade9f342c6a9c366ccb0ab3f93a49738f3217b6e
David Britch <davidbritch@users.noreply.github.com>
2021-03-18 09:42:25 +0000

    ARKit animation sample.

---
+++ ARKit/05 - ARKitFun - Animation/ARKitFun/Extensions/SCNNodeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SceneKit;
+
+namespace ARKitFun.Extensions
+{
+    public static class SCNNodeExtensions
+    {
+        public static void AddRotationAction(this SCNNode node, SCNActionTimingMode mode, double secs, bool loop = false)
+        {
+            SCNAction rotateAction = SCNAction.RotateBy(0, (float)Math.PI, 0, secs);
+            rotateAction.TimingMode = mode;
+
+            if (loop)
+            {
+                SCNAction indefiniteRotation = SCNAction.RepeatActionForever(rotateAction);
+                node.RunAction(indefiniteRotation, "rotation");
+            }
+            else
+                node.RunAction(rotateAction, "rotation");
+        }
+    }
+}
